aafe36bd6a25c3fc8b2580972ca04e478cb6321e
janetyc <janetyc@gmail.com>
2014-03-09 16:39:28 +0800

    use app.debug

--- run.py
+++ run.py
@@ -2,5 +2,4 @@ import os
 from crowdapp import app
 
 port = int(os.environ.get('PORT', 5000))
-print app.debug
-app.run(host='0.0.0.0', port=port, debug=True)
+app.run(host='0.0.0.0', port=port, debug=app.debug)
